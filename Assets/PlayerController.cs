d52e9759e9095ee799f87157a53ff8373d3c3b32
Ali Muhammad Kashif <hamkuas@gmail.com>
2019-04-17 14:49:16 +0200

    Test1
    Test1

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 100;
     private Rigidbody2D rb;
 
+    public PlayerController()
+    {
+    }
+
     // Start is called before the first frame update
     void Start()
     {
