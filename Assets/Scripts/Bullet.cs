84947bdd006803188c8dc1b163603ecd238b13c0
Ali Muhammad Kashif <46288529+alimuhammadkashif@users.noreply.github.com>
2019-04-24 13:36:41 +0200

    Bullet Script

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bullet : MonoBehaviour
+
+{   // Start is called before the first frame update   
+
+    public float bulletSpeed = 10;
+    public Rigidbody bullet;
+
+
+    void Fire()
+    {
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.forward * bulletSpeed;
+    }
+    
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+    }
+}
+
+
+
