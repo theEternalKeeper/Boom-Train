845a4059901534592fce936a3de53620a413bf45
Ali Muhammad Kashif <hamkuas@gmail.com>
2019-04-24 15:41:17 +0200

    Bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Bullet : MonoBehaviour
+public class ShootDemo : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float speed = 20;
 
-{   // Start is called before the first frame update   
-
-    public float bulletSpeed = 10;
-    public Rigidbody bullet;
-
-
-    void Fire()
-    {
-        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
-        bulletClone.velocity = transform.forward * bulletSpeed;
-    }
-    
     // Update is called once per frame
-
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
-            Fire();
+        {
+            Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+        }
     }
 }
-
-
-
