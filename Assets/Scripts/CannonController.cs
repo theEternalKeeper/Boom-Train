fe9a21e6026428045d6bba5d6f8b5e9f49f872eb
Ali Muhammad Kashif <46288529+alimuhammadkashif@users.noreply.github.com>
2019-04-25 08:18:44 +0200

    CannonControllers

---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CannonController : MonoBehaviour
+{
+
+    public Rigidbody2D cannonRB;
+    //public GameObject cannon;
+    public Transform cannonRotation;
+    public bool controlled;
+
+    public GameObject shotLocation;
+    public Vector2 shootLocation;
+
+    public GameObject projectile;
+    public Rigidbody2D rb;
+    public Collider2D projcollider;
+    public float speed;
+
+    private int timeoutDestructor;
+
+    // Use this for initialization
+    void Start()
+    {
+        cannonRotation = GetComponent<Transform>();
+        cannonRB = GetComponent<Rigidbody2D>();
+        //cannon = GetComponent<GameObject> ();
+        controlled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootLocation = new Vector2(shotLocation.transform.position.x, shotLocation.transform.position.y);
+        if (controlled == true)
+        {
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward * 100 * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.back * 100 * Time.deltaTime);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //timeoutDestructor = 1;
+                Launch();
+                //controlled = false;
+            }
+
+        }
+    }
+
+    void Launch()
+    {
+        //cannon = GetComponent<GameObject> ();
+        GameObject clone = Instantiate(projectile, shootLocation, Quaternion.identity) as GameObject;
+        Rigidbody2D clonerb = clone.GetComponent<Rigidbody2D>();
+        clonerb.AddRelativeForce(transform.TransformDirection(new Vector2((Mathf.Cos(transform.rotation.z * Mathf.Deg2Rad) * speed),
+                                                                             (Mathf.Sin(transform.rotation.z * Mathf.Deg2Rad) * speed))),
+                                                                             ForceMode2D.Impulse);
+    }
+
+
+}
+
+
