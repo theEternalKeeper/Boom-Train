de9ef38aae492777a38c258ab8809582cba6832d
Oskar Bergman <kemioskar@gmail.com>
2019-05-14 15:08:09 +0200

    Menu
    Makes the scene folder tidier
    
    added targets to second leg
    
    Added functional main menu

---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public string Scene1;
+    public string Scene2;
+    public string Scene3;
+    public void NextScene1()
+    {
+        SceneManager.LoadScene(Scene1);
+    }
+    public void NextScene2() { SceneManager.LoadScene(Scene2); }
+    public void NextScene3() { SceneManager.LoadScene(Scene3); }
+}
+//tst
