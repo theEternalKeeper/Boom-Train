6bb477d4fce7938f241e094f7bd588d96eef8b54
Christoffer Öberg <christofferoberg95@gmail.com>
2019-05-14 15:38:51 +0200

    Updated Scoretext UI

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreDisplay : MonoBehaviour
-{   
+{
+    GameObject scoreSystem;
+    ScoreSystem_Core scoreSystem_Core;
+    Text scoreText;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreSystem = GameObject.Find("ScoreSystem");
+        scoreSystem_Core = scoreSystem.GetComponent<ScoreSystem_Core>();
+        scoreText = gameObject.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "Current Score: " + scoreSystem_Core.currentScore;
     }
 }
