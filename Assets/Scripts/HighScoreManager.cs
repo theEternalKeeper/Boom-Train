7d4898f46c7eb340781474a418b33fb2771f6040
Ali Muhammad Kashif <hamkuas@gmail.com>
2019-05-16 13:37:50 +0200

    Score Screen
    For scoring 1, press space key.

--- Assets/Scripts/HighScoreManager.cs
+++ Assets/Scripts/HighScoreManager.cs
@@ -1,138 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
- 
- /// <summary>
- /// High score manager.
- /// Local highScore manager for LeaderboardLength number of entries
- /// 
- /// this is a singleton class.  to access these functions, use HighScoreManager._instance object.
- /// eg: HighScoreManager._instance.SaveHighScore("meh",1232);
- /// No need to attach this to any game object, thought it would create errors attaching.
- /// </summary>
- 
- public class HighScoreManager : MonoBehaviour
+public class HighScoreManager : MonoBehaviour
 {
 
-    private static HighScoreManager m_instance;
-    private const int LeaderboardLength = 10;
+    int Score, HighScore;
+    public Text Scoretext, HighScoretext;
+    
 
-    public static HighScoreManager _instance
+    // Use this for initalization 
+
+    void Start()
     {
-        get
-        {
-            if (m_instance == null)
-            {
-                m_instance = new GameObject("HighScoreManager").AddComponent<HighScoreManager>();
-            }
-            return m_instance;
-        }
+        HighScore = PlayerPrefs.GetInt("HighScore, 0");
     }
+    
 
-    void Awake()
+    // Update is called per frame 
+
+    void Update()
     {
-        if (m_instance == null)
-        {
-            m_instance = this;
-        }
-        else if (m_instance != this)
-            Destroy(gameObject);
+        if (Input.GetKeyDown("space"))
 
-        DontDestroyOnLoad(gameObject);
-    }
 
-    public void SaveHighScore(string name, int score)
-    {
-        List<Scores> HighScores = new List<Scores>();
 
-        int i = 1;
-        while (i <= LeaderboardLength && PlayerPrefs.HasKey("HighScore" + i + "score"))
         {
-            Scores temp = new Scores();
-            temp.score = PlayerPrefs.GetInt("HighScore" + i + "score");
-            temp.name = PlayerPrefs.GetString("HighScore" + i + "name");
-            HighScores.Add(temp);
-            i++;
-        }
-        if (HighScores.Count == 0)
-        {
-            Scores _temp = new Scores();
-            _temp.name = name;
-            _temp.score = score;
-            HighScores.Add(_temp);
-        }
-        else
-        {
-            for (i = 1; i <= HighScores.Count && i <= LeaderboardLength; i++)
-            {
-                if (score > HighScores[i - 1].score)
-                {
-                    Scores _temp = new Scores();
-                    _temp.name = name;
-                    _temp.score = score;
-                    HighScores.Insert(i - 1, _temp);
-                    break;
-                }
-                if (i == HighScores.Count && i < LeaderboardLength)
-                {
-                    Scores _temp = new Scores();
-                    _temp.name = name;
-                    _temp.score = score;
-                    HighScores.Add(_temp);
-                    break;
-                }
-            }
+            Score = Score + 1;
+
         }
 
-        i = 1;
-        while (i <= LeaderboardLength && i <= HighScores.Count)
+        Scoretext.text = "Score:" + Score.ToString();
+        HighScoretext.text = "HighScore:" + HighScore.ToString();
+
+
+        if (Score > HighScore)
         {
-            PlayerPrefs.SetString("HighScore" + i + "name", HighScores[i - 1].name);
-            PlayerPrefs.SetInt("HighScore" + i + "score", HighScores[i - 1].score);
-            i++;
+
+            HighScore = Score;
+            PlayerPrefs.SetInt("HighScore", HighScore);
+
         }
 
     }
-
-    public List<Scores> GetHighScore()
-    {
-        List<Scores> HighScores = new List<Scores>();
-
-        int i = 1;
-        while (i <= LeaderboardLength && PlayerPrefs.HasKey("HighScore" + i + "score"))
-        {
-            Scores temp = new Scores();
-            temp.score = PlayerPrefs.GetInt("HighScore" + i + "score");
-            temp.name = PlayerPrefs.GetString("HighScore" + i + "name");
-            HighScores.Add(temp);
-            i++;
-        }
-
-        return HighScores;
-    }
-
-    public void ClearLeaderBoard()
-    {
-        //for(int i=0;i<HighScores.
-        List<Scores> HighScores = GetHighScore();
-
-        for (int i = 1; i <= HighScores.Count; i++)
-        {
-            PlayerPrefs.DeleteKey("HighScore" + i + "name");
-            PlayerPrefs.DeleteKey("HighScore" + i + "score");
-        }
-    }
-
-    void OnApplicationQuit()
-    {
-        PlayerPrefs.Save();
-    }
-}
-
-public class Scores
-{
-    public int score;
-    public string name;
-
 }
